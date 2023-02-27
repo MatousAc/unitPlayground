75090c99815664a9b9774998feb2c773cd946bc5
Ac Hybl <matoush@southern.edu>
2023-02-27 01:36:56 -0500

    better error handling

--- src/js/equation.js
+++ src/js/equation.js
@@ -2,47 +2,65 @@
 import settings from './settings'
 import { unit } from './stores'
 import { typeOf } from './helpers'
-import { Fail, Hint, UnrecognizedUnit } from './error'
+import { NonError, Fail, Hint, MissingOperand, UnitMismatch, UnrecognizedUnit } from './error'
 
 let sigFigs, useScalar, simplify, system;
 settings.subscribe(s => {
-  useScalar = s.includeScalar;
-  sigFigs = s.precision;
-  simplify = s.simplify;
-  system = s.system;
+  useScalar = s.includeScalar
+  sigFigs = s.precision
+  simplify = s.simplify
+  system = s.system
 })
 
 // json AST => single result
-export let getResultUnits = (json, fallbackValue) => {
-  if (inProgress(JSON.stringify(json))) {
-    return fallbackValue
-  }
-  let res = ''
+export let getResultUnits = (json, currentResult) => {
+  if (inProgress(json)) return currentResult
+
+  let result = ''
   try {
-    let qty = converge(json)
-    console.log('Unit', qty)
-    // if (simplify) qty = qty.simplify();
-    res = toLaTeX(qty)
-    console.log('Result', res)
+    let value = converge(json)
+    console.log('Unit', value)
+    console.log("unit object when being used")
+    console.log(unit.definitions())
+    if (simplify) value = value.simplify();
+    result = toLaTeX(value)
+    console.log('Result:', result)
   } catch (e) {
-    console.error(e)
+    switch (e.constructor) {
+    case NonError: console.warn(e.message); break
+    case UnitMismatch:
+      console.error(e.message); break
+    case UnrecognizedUnit:
+      console.error(e.message)
+      console.error("Define new unit?"); break
+    case MissingOperand: console.error(e.message); break
+    case Hint:
+      console.error(e.message)
+      console.error("You might wanna czech this one out : )")
+      break
+    case Fail:
+      console.error(e.message)
+      console.error("User failed."); break
+    default:
+      console.error(e)
+    }
   }
 
-  return res == '' ? fallbackValue : '=' + res
+  return result == '' ? currentResult : '=' + result
 }
 
-// recursively drills through a json AST, returns a Unit
+// recursively processes json AST, returns a Unit
 let converge = ast => {
   // console.log("Ast:", ast)
   switch (typeOf(ast)) {
-    case 'array': break
-    case 'unit': return ast
-    case 'number': return unit(ast)
-    case 'string':
-      throw new UnrecognizedUnit(ast)
-    case 'object':
-    default:
-      return ast
+  case 'array': break
+  case 'unit': return ast
+  case 'number': return unit(ast)
+  case 'string':
+    throw new UnrecognizedUnit(ast)
+  case 'object':
+  default:
+    return ast
   }
 
   let op = ast[0]
@@ -54,19 +72,26 @@ let converge = ast => {
     return ast.slice(1).reduce((a, b) => {
       a = converge(a)
       b = converge(b)
-      return eval(`a.${op.toLowerCase().substring(0, 3)}(b)`)
+      try {
+        return eval(`a.${op.toLowerCase().substring(0, 3)}(b)`)
+      } catch (e) {
+        if (e.message.includes("dimensions do not match")) {
+          throw new UnitMismatch(e.message)
+        } else throw e
+      }
   })
+  case "Sequence":
+    if (ast.length == 1) throw new NonError()
+    return ast.slice(1).forEach(member => {
+      converge(member) // till we hit an error
+    });
   
-  case 'Power':
-    return power(ast)
-  case 'Sqrt':
-    return converge(ast[1]).sqrt()
-  case 'UNIT':
-    return unit(ast[1])
-  case 'List':
-    return ast.slice(1)
-  default:
-    return ''
+  case 'Power': return power(ast)
+  case 'Sqrt': return converge(ast[1]).sqrt()
+  case 'UNIT': return unit(ast[1])
+  case 'List': return ast.slice(1)
+  case "Error": return handleError(ast)
+  default: return ''
   }
 }
 
@@ -104,16 +129,34 @@ let toLaTeX = u => {
   return scalar + ' ' + uStr  
 }
 
-function inProgress(jstr) {
+function inProgress(json) {
+  let str = JSON.stringify(json)
   return (
-    jstr == '' ||
-    jstr.includes('Error') ||
-    jstr.includes('missing') ||
-    jstr.includes('Sequence') ||
-    jstr.includes('Nothing') ||
-    jstr.includes('HorizontalSpacing')
+    str == '' ||
+    str.includes('Nothing') ||
+    str.includes('HorizontalSpacing')
   ) // might be nice to handle various errors here
 }
 
+// handed an ast, this determines the proper error
+const handleError = ast => {
+  console.log("Error AST", ast)
+  if (ast[0] === "Error") {
+    if (typeOf(ast[1]) === "array") handleError(ast[1])
+    else if (typeOf(ast[1]) === "string") {
+      if (ast[1] === "'missing'") throw new MissingOperand()
+    }
+  } else if (ast[0] === "ErrorCode") {
+    let code = ast[1]
+    switch (code) {
+    case "'unexpected-command'":
+      let re = new RegExp(/["|']\\(?<name>.*)["|']/)
+      let badUnit = ast[2].match(re).groups.name
+      throw new UnrecognizedUnit(badUnit)
+    default:
+      return ''
+    }
+  }
+}
+
 export const eqKey = Symbol()
-export const precKey = Symbol()
--- src/js/error.js
+++ src/js/error.js
@@ -1,21 +1,52 @@
 // custom error types
-export class Fail extends Error {
+export class NonError {
   constructor(message) {
-    super(message);
-    this.name = 'Fail';
+    this.name = this.constructor.name;
+    this.message = "Stop parsing for now.";
+    this.stack = (new Error()).stack;
   }
 }
 
-export class Hint extends Error {
+export class Fail {
   constructor(message) {
-    super(message);
-    this.name = 'Hint';
+    this.name = this.constructor.name;
+    this.message = message;
+    this.stack = (new Error()).stack;
+  }
+}
+
+export class Hint {
+  constructor(message) {
+    this.name = this.constructor.name;
+    this.message = message;
+    this.stack = (new Error()).stack;
   }
 }
 
 export class UnrecognizedUnit extends Hint {
-  constructor(str) {
-    super(`Unit '${str}' not recognized.`);
-    this.name = 'UnrecognizedUnit';
+  constructor(unitString) {
+    super(`Unit '${unitString}' not recognized.`);
+    this.name = this.constructor.name;
+  }
+}
+
+export class UnitMismatch extends Hint {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+export class UnrecognizedCharacters extends Hint {
+  constructor(chars) {
+    super(`We don't know what the character ${chars} means.`);
+    this.name = this.constructor.name;
+  }
+}
+
+export class MissingOperand extends Hint {
+  constructor() {
+    super(`You are missing an operand.`);
+    this.name = this.constructor.name;
   }
 }
